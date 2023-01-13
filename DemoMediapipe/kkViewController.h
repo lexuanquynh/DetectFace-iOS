f04663e0222576982112ab1c83847e4141c68bd6
lexuanquynh <le-xuan-quynh@goodcycle.net>
2023-01-13 11:50:46 +0700

    add demo

---
+++ DemoMediapipe/kkViewController.h
@@ -0,0 +1,16 @@
+//
+//  kkViewController.h
+//  DemoMediapipe
+//
+//  Created by Le Xuan Quynh on 13/01/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface kkViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
